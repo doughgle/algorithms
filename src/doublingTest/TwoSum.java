7764cc7f769ff110b91f207035e0c4310a8458a4
Douglas Hellinger <doug.hellinger@hotmail.com>
2014-09-20 10:58:18 +0800

    Updated comments to describe the Two Sum quadratic algorithm.

--- src/doublingTest/TwoSum.java
+++ src/doublingTest/TwoSum.java
@@ -3,13 +3,13 @@ package doublingTest;
 public class TwoSum {
 	
 	/**
-	 * Counts the number of triples in the given integer array which sum to zero.
-	 * i.e. a + b + c = 0.
+	 * Counts the number of pairs in the given integer array which sum to zero.
+	 * i.e. a[i] + a[j] = 0.
 	 * 
-	 * The order of growth is N^3.
+	 * The order of growth is N^2.
 	 * A mathematic model of the running time is:
 	 * 
-	 * 	T(N) ~ aN^3
+	 * 	T(N) ~ aN^2
 	 * 
 	 * where a is some machine dependent constant.
 	 * 
@@ -45,14 +45,8 @@ public class TwoSum {
 	 * 			+ t(B) * N
 	 * 	  	time to execute C statements multiplied by frequency of execution
 	 * 			+ t(C) * N^2
-	 * 	 	time to execute D statements multiplied by frequency of execution
-	 * 			+ t(D) * N^3
 	 * 		time to execute input dependent statements
 	 * 			+ t(E) * x ...where x is dependent upon the input data
 	 */
 	
-	/**
-	 * Tilde approximation of the total running time = ~(t(D)/6)N^3
-	 * i.e. the time it takes to execute D statements * N, assuming x is small. 
-	 */
 }
